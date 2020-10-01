b546d5e1f176c01a29eb5f0388f640cfca647386
biancacraciun <craciunbiancas@yahoo.com>
2020-10-01 17:26:54 +0300

    reusable password input

--- src/commons/util/password/password.js
+++ src/commons/util/password/password.js
@@ -1,24 +1,41 @@
 import React, { useState } from "react";
-// import "../../../pages/Sign-In/sign-in.css";
+import Error from "../error/error";
+import { Eye, EyeOff, Check } from "react-feather";
 import "../inputs.css";
 
 const Password = (props) => {
+  const [inputType, changeInputType] = useState("password");
+
   return (
     <div className="form-section">
       <label htmlFor="password">
         {props.label}: <span className="is-required">*</span>
       </label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        onChange={props.password}
-      />
-      {props.isError ? (
-        <span className="errors">
-          Password must contain at least one number and one uppercase and
-          lowercase letter, and at least 7 or more characters
-        </span>
+      <div className="input">
+        <input
+          type={inputType}
+          name="password"
+          id={props.id}
+          onChange={props.password}
+        />
+        <button
+          type="button"
+          onClick={() =>
+            inputType === "password"
+              ? changeInputType("text")
+              : changeInputType("password")
+          }
+          className="see-password"
+        >
+          {inputType === "password" ? <EyeOff /> : <Eye />}
+        </button>
+      </div>
+      {props.isError ? <Error /> : null}
+      {props.isValid === false ? (
+        <Error errorMessage="Passwords must be at least 7 characters in length, but can be much longer, must be a minimum of 1 lower case letter, a minimum of 1 upper case letter, a minimum of 1 numeric character and a minimum of 1 special character." />
+      ) : null}
+      {props.isConfirmation ? (
+        <Error errorMessage="Please add the same value again." />
       ) : null}
     </div>
   );
@@ -26,5 +43,11 @@ const Password = (props) => {
 
 Password.defaultProps = {
   label: "Password",
+  id: "password",
+  errorMessage: "This is a required field.",
+  passwordErr: null,
+  noConfirmation: null,
+  isConfirmation: null,
+  isValid: null,
 };
 export default Password;
