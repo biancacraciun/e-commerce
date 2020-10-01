fd851e1dd8bdf4c8960f4991fc2f3fae94255e13
biancacraciun <craciunbiancas@yahoo.com>
2020-10-01 17:25:55 +0300

    reusable input

--- src/commons/util/basic-input/basic-input.js
+++ src/commons/util/basic-input/basic-input.js
@@ -1,15 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Error from "../error/error";
 
 const BasicInput = (props) => {
   return (
     <div className="form-section">
       <label htmlFor={props.for}>
-        {props.forText}
-        <span className="is-required">*</span>
+        {props.forText}: <span className="is-required">*</span>
       </label>
-      <input type="text" id={props.for} name={props.for} />
+      <input
+        type="text"
+        id={props.for}
+        name={props.for}
+        onChange={props.dataHandler}
+      />
+      {props.isError ? (
+        <Error errorMessage="This is a required field." />
+      ) : null}
     </div>
   );
+};
+
+BasicInput.propTypes = {
+  for: PropTypes.string,
+  forText: PropTypes.string,
+  dataHandler: PropTypes.func,
+  isError: PropTypes.bool,
 };
 
 export default BasicInput;
