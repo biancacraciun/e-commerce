cac13be0f498bdbd1b3c437c9dc77dac5bd05434
biancacraciun <craciunbiancas@yahoo.com>
2020-10-01 18:47:06 +0300

    change password route

--- src/pages/Change-Password/change-password.js
+++ src/pages/Change-Password/change-password.js
@@ -6,7 +6,7 @@ import "./change-password.css";
 const ChangePassword = () => {
   return (
     <div className="change-password">
-      <h1 className="title">Verify your email address!</h1>
+      <h1 className="title ">Verify your email address!</h1>
       <p className="content">
         Just check your email and follow the link to finish change your
         password.
