b3c174851c7eee71e627b4b45d8c0ef8d470146a
biancacraciun <craciunbiancas@yahoo.com>
2020-10-07 20:55:59 +0300

    my-account component after login

---
+++ src/pages/My-Account/my-account.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Card from "../../commons/util/cards/card";
+import "./my-account.css";
+
+const MyAccount = () => {
+  return (
+    <div className="my-account">
+      <section className="account-information">
+        <p className="titles" role="subtitle">
+          Account Information
+        </p>
+        <Card title="CONTACT INFORMATION" change="Change Password" />
+      </section>
+      <section className="address-book">
+        <div className="cards-title">
+          <p className="titles" role="subtitle">
+            Address Book
+          </p>
+          <Link to="/" className="address-manager">
+            Manage Addresses
+          </Link>
+        </div>
+        <div className="cards">
+          <Card
+            title="DEFAULT BILING ADDRESS"
+            more="Address"
+            data="You have not set a default billing address."
+          />
+          <Card
+            title="DEFAULT SHIPPING ADDRESS"
+            more="Address"
+            data="You have not set a default shipping address."
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default MyAccount;
