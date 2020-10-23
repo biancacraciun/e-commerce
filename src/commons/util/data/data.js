a50c60341c87c5425713159b6778225679bdf4d5
biancacraciun <craciunbiancas@yahoo.com>
2020-10-23 10:07:55 +0300

    data arrays

---
+++ src/commons/util/data/data.js
@@ -0,0 +1,21 @@
+export const gender = [
+  "New Arrivals",
+  "Men",
+  "Woman",
+  "Girls",
+  "Boys",
+  "Unisex",
+];
+
+export const products = ["Shoes", "Clothes", "Accessories"];
+export const types = [
+  "Lifestyle",
+  "Running",
+  "Trail-Running",
+  "Yoga",
+  "Fitness",
+  "Cycling",
+  "Training",
+];
+
+export const pages = ["About us", "Contact us", "Shopping cart", "My account"];
