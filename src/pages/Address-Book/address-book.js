f1f51c46c17299a14ced730ee1b4e40c0dce06f0
biancacraciun <craciunbiancas@yahoo.com>
2020-10-23 10:05:37 +0300

    AddressBook

---
+++ src/pages/Address-Book/address-book.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../Layouts/layout/layout";
+import Container from "../Layouts/container/container";
+
+const AddressBook = () => {
+  return (
+    <Container>
+      <Layout subtitle="CONTACT INFORMATION"></Layout>
+    </Container>
+  );
+};
+
+export default AddressBook;
