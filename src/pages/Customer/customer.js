2abeb795dd601467facb033c95e1849d5d569ea2
biancacraciun <craciunbiancas@yahoo.com>
2020-10-23 10:05:08 +0300

    Customer

---
+++ src/pages/Customer/customer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import LoggedIn from "../Logged-In/logged-in";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+
+const Customer = (props) => {
+  return <div className="customer"></div>;
+};
+
+export default Customer;
