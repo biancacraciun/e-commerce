d027b1d060b8fe7579da503ad213e16a341acb19
biancacraciun <craciunbiancas@yahoo.com>
2020-10-23 09:59:33 +0300

    Wishlist

---
+++ src/pages/My-Wishlist/my-wishlist.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import AuthPage from "../../commons/util/components/auth-page/auth-page";
+
+const MyWishList = () => {
+  return <AuthPage alertMessage="You have no items in your wish list." />;
+};
+
+export default MyWishList;
