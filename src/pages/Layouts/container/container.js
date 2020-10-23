8640c94bb0fb7465bb1cfdaff1ed85ea5edc38a4
biancacraciun <craciunbiancas@yahoo.com>
2020-10-23 10:01:43 +0300

    Container without defaultProps

--- src/pages/Layouts/container/container.js
+++ src/pages/Layouts/container/container.js
@@ -4,15 +4,13 @@ import "./container.css";
 const Container = (props) => {
   return (
     <div className="container">
-      <p className="container-title">{props.title}</p>
+      <p className="container-title" role="title">
+        {props.title}
+      </p>
 
       <div className="content">{props.children}</div>
     </div>
   );
 };
 
-Container.defaultProps = {
-  title: null,
-  children: null,
-};
 export default Container;
