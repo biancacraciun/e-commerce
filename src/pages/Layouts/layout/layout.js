fcefcbde77129f88baab846eb16cfaabe3e2e5ee
biancacraciun <craciunbiancas@yahoo.com>
2020-10-23 10:00:59 +0300

    Add Form component to Layout (reusable)

--- src/pages/Layouts/layout/layout.js
+++ src/pages/Layouts/layout/layout.js
@@ -1,40 +1,21 @@
 import React from "react";
+
 import "./layout.css";
+import Form from "../../../commons/util/components/forms/form/form";
 
 const Layout = (props) => {
   return (
-    <div className={props.className === "grid" ? "info full-width" : "info"}>
-      <span className="subtitle" role="title">
+    <div className={props.className === "grid" ? "full-width info" : "info"}>
+      <span className="subtitle" role="subtitle">
         {props.subtitle}
       </span>
       <span className="details">{props.details}</span>
 
-      <form
-        className={`register-form ${props.className}`}
-        onSubmit={props.handleChange}
-      >
+      <Form handleChange={props.handleChange} buttonVal={props.buttonVal}>
         {props.children}
-
-        <div className="bottom">
-          <input
-            type="submit"
-            role="button"
-            value={props.value}
-            className="submit-button"
-          />
-          {props.link}
-        </div>
-      </form>
-
-      <span className="is-required">* Required Fields</span>
+      </Form>
     </div>
   );
 };
 
-Layout.defaultProps = {
-  subtitle: null,
-  details: null,
-  link: null,
-  className: null,
-};
 export default Layout;
