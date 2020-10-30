44ce538dd39d47c2336a751222b5349db527fd91
biancacraciun <craciunbiancas@yahoo.com>
2020-10-30 14:14:42 +0200

    Home Page update

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,35 +1,33 @@
 import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  BrowserRouter,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   BrowserRouter,
+// } from "react-router-dom";
 
-import Header from "../../commons/containers/header/header";
+// import Header from "../../commons/containers/header/header";
 import Products from "../../pages/Products/products";
 import Slider from "../../commons/components/slideshow/slider";
 import RecentlyViewedSection from "../../commons/containers/recently-viewed-section/recently-viewed-section";
-import Footer from "../../commons/containers/footer-container/footer";
+// import Footer from "../../commons/containers/footer-container/footer";
 
 import "./home.css";
 
 const Home = (props) => {
   return (
     <>
-      <Header openSideDrawer={props.openSideDrawer} />
+      {/* <Header openSideDrawer={props.openSideDrawer} /> */}
       <div className="landing-page" />
 
       <div className="display-products">
-        <Route exact path="/">
-          <Products />
-        </Route>
+        <Products />
       </div>
 
       <Slider />
       <RecentlyViewedSection />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
