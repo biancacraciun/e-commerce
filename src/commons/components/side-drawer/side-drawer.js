9f129b06a50dbd12c0f1fe6a163c912d31786d94
biancacraciun <craciunbiancas@yahoo.com>
2020-10-30 14:11:26 +0200

    SideDrawer

--- src/commons/components/side-drawer/side-drawer.js
+++ src/commons/components/side-drawer/side-drawer.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory, Route } from "react-router-dom";
 
-import ResponsiveNavigation from "./responsive-navigation/responsive-navigation";
+import ResponsiveMenu from "./responsive-menu/responsive-menu";
 import SideDrawerMenu from "./side-drawer-menu/side-drawer-menu";
+import ResponsiveAccountNavigation from "./responsive-account-navigation/responsive-account-navigation";
 import "./side-drawer.css";
 
 const SideDrawer = (props) => {
@@ -11,10 +13,22 @@ const SideDrawer = (props) => {
     classes = "side-drawer open";
   }
 
+  const [isMenuClicked, clickMenu] = useState(true);
+  const [isAccoundClicked, clickAccount] = useState(false);
+
+  const menuClickHandler = () =>
+    isAccoundClicked ? clickMenu(false) : clickMenu(true);
+
+  const accountClickHandler = () =>
+    isMenuClicked ? clickMenu(false) : clickAccount(true);
+
   return (
     <div className={classes}>
-      <SideDrawerMenu />
-      <ResponsiveNavigation />
+      <SideDrawerMenu
+        menuClickHandler={menuClickHandler}
+        accountClickHandler={accountClickHandler}
+      />
+      {isMenuClicked ? <ResponsiveMenu /> : <ResponsiveAccountNavigation />}
     </div>
   );
 };
