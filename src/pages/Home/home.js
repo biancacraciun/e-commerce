961b02d05e0ec487c664093022643ed72393c0f2
biancacraciun <craciunbiancas@yahoo.com>
2020-10-31 12:38:35 +0200

    without router

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,13 +1,5 @@
 import React, { useState } from "react";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link,
-//   BrowserRouter,
-// } from "react-router-dom";
 
-// import Header from "../../commons/containers/header/header";
 import Products from "../../pages/Products/products";
 import Slider from "../../commons/components/slideshow/slider";
 import RecentlyViewedSection from "../../commons/containers/recently-viewed-section/recently-viewed-section";
@@ -18,7 +10,6 @@ import "./home.css";
 const Home = (props) => {
   return (
     <>
-      {/* <Header openSideDrawer={props.openSideDrawer} /> */}
       <div className="landing-page" />
 
       <div className="display-products">
@@ -27,7 +18,6 @@ const Home = (props) => {
 
       <Slider />
       <RecentlyViewedSection />
-      {/* <Footer /> */}
     </>
   );
 };
