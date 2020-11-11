7a47cdd2b46c5949f116fd63678af018dc1e9a5c
biancacraciun <craciunbiancas@yahoo.com>
2020-11-11 10:21:41 +0200

    categories hooksk

--- src/commons/components/categories/categories.js
+++ src/commons/components/categories/categories.js
@@ -1,100 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { checkboxes } from "../../util/data/data";
+import Checkbox from "../../util/inputs/checkbox/checkbox";
+
 import "./categories.css";
 
-const Categories = () => {
+const Categories = (props) => {
+  // const [criteria, setCriteria] = useState([]);
+  const [criteria, setCriteria] = useState("");
+
+  const valHandler = (data, isChecked) => {
+    if (isChecked) {
+      // setCriteria(criteria.concat(data));
+      setCriteria(data);
+    }
+  };
+
+  useEffect(() => {
+    if (criteria !== "") {
+      props.selectedData(criteria);
+    }
+  });
+
   return (
     <div className="all-categories">
       <span className="container-title">SHOP BY:</span>
       <nav className="options">
         <ul className="options-list">
-          <li>
-            <input
-              type="checkbox"
-              id="new-arrivals"
-              name="new-arrivals"
-              value="New arrivals"
-              className="checkbox"
-            />
-            <span className="new-checkbox" />
-            <label htmlFor="new-arrivals"> New arrivals </label>
-          </li>
-          <li>
-            <input
-              type="checkbox"
-              id="men"
-              name="men"
-              value="Men"
-              className="checkbox"
-            />
-            <span className="new-checkbox" />
-            <label htmlFor="men"> Men </label>
-          </li>
-          <li>
-            <input
-              type="checkbox"
-              id="women"
-              name="women"
-              value="Women"
-              className="checkbox"
-            />
-            <span className="new-checkbox" />
-            <label htmlFor="women"> Women </label>
-          </li>
-          <li>
-            <input
-              type="checkbox"
-              id="boys"
-              name="boys"
-              value="Boys"
-              className="checkbox"
-            />
-            <span className="new-checkbox" />
-            <label htmlFor="boys"> Boys </label>
-          </li>
-          <li>
-            <input
-              type="checkbox"
-              id="girls"
-              name="girls"
-              value="Girls"
-              className="checkbox"
-            />
-            <span className="new-checkbox" />
-            <label htmlFor="girls"> Girls </label>
-          </li>
-          <li>
-            <input
-              type="checkbox"
-              id="shoes"
-              name="shoes"
-              value="Shoes"
-              className="checkbox"
-            />
-            <span className="new-checkbox" />
-            <label htmlFor="shoes"> Shoes </label>
-          </li>
-          <li>
-            <input
-              type="checkbox"
-              id="clothes"
-              name="chlotes"
-              value="Clothes"
-              className="checkbox"
-            />
-            <span className="new-checkbox" />
-            <label htmlFor="clothes"> Clothes </label>
-          </li>
-          <li>
-            <input
-              type="checkbox"
-              id="accessories"
-              name="accessories"
-              value="Accessories"
-              className="checkbox"
-            />
-            <span className="new-checkbox" />
-            <label htmlFor="accessories"> Accessories </label>
-          </li>
+          {checkboxes.map((checkbox) => {
+            return (
+              <Checkbox
+                key={checkbox.id}
+                valFn={valHandler}
+                id={checkbox.id}
+                name={checkbox.name}
+                value={checkbox.value}
+              />
+            );
+          })}
         </ul>
       </nav>
     </div>
