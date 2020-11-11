0a1edffdb01fd39ae6faf3c961edc87d3a6d6c8b
biancacraciun <craciunbiancas@yahoo.com>
2020-11-11 10:15:53 +0200

    load-more button

---
+++ src/commons/util/buttons/load-more-button/load-more-button.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./load-more-button.css";
+
+const LoadMore = (props) => {
+  return <button className="load-more">LOAD MORE</button>;
+};
+
+export default LoadMore;
