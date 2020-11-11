4653d8df6356d09f532dea6387cf1f4d8a8f0acc
biancacraciun <craciunbiancas@yahoo.com>
2020-11-11 09:58:08 +0200

    products pass data

--- src/pages/Products/products.js
+++ src/pages/Products/products.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import Categories from "../../commons/components/categories/categories";
 import Data from "../../commons/containers/data-products/data-products";
+import "./products.css";
 
 const Products = () => {
+  const [data, setData] = useState(null);
+
+  const getSelectedData = (items) => {
+    setData(items);
+  };
+
+  // console.log("isSelected", data);
   return (
     <div className="display-products">
-      <Categories />
-      <Data />
+      <Categories selectedData={getSelectedData} />
+      <Data selected={data} />
     </div>
   );
 };
