df42b1447aa0233bfdc74d55033f11c7a1cf40e4
biancacraciun <craciunbiancas@yahoo.com>
2020-11-11 09:59:17 +0200

    home change structure

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -3,22 +3,19 @@ import React, { useState } from "react";
 import Products from "../../pages/Products/products";
 import Slider from "../../commons/components/slideshow/slider";
 import RecentlyViewedSection from "../../commons/containers/recently-viewed-section/recently-viewed-section";
-// import Footer from "../../commons/containers/footer-container/footer";
 
 import "./home.css";
 
 const Home = (props) => {
   return (
-    <>
+    <div>
       <div className="landing-page" />
 
-      <div className="display-products">
-        <Products />
-      </div>
+      <Products />
 
       <Slider />
       <RecentlyViewedSection />
-    </>
+    </div>
   );
 };
 
