ccdd59c4af02ed9e1fbf78409dbf6e86d53c1fe2
biancacraciun <craciunbiancas@yahoo.com>
2020-11-11 10:16:30 +0200

    data-products add hooks

--- src/commons/containers/data-products/data-products.js
+++ src/commons/containers/data-products/data-products.js
@@ -1,33 +1,60 @@
-import { response } from "express";
-import res from "express/lib/response";
-import React, { Component } from "react";
-import Product from "../../components/product/product";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
+import Product from "../../components/product/product";
+import LoadMore from "../../util/buttons/load-more-button/load-more-button";
 import "./data-products.css";
 
-class Data extends Component {
-  state = {
-    data: [],
-  };
+const Data = (props) => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState([]);
 
-  componentDidMount() {
-    // fetch("http://localhost:8080/products")
-    //   .then((response) => response.json())
-    //   .then(
-    //     (response) => console.log(response)
-    //     // this.setState({
-    //     //   data: response
-    //     // })
-    //   );
-  }
+  useEffect(() => {
+    fetch("http://localhost:8080/products")
+      .then((response) => response.json())
+      .then((response) => setData(response));
+  }, []);
 
-  render() {
-    return (
-      <section className="products">
-        <Product />
-      </section>
-    );
-  }
-}
+  useEffect(() => {
+    setFilter(props.selected);
+  });
+  console.log(filter);
+
+  return (
+    <section className="products">
+      {filter === null || filter === []
+        ? data.map((product) => {
+            return (
+              <Product
+                img={product.image[0]}
+                productName={product.model}
+                productPrice={product.price}
+              />
+            );
+          })
+        : data.filter((item) => {
+            console.log(filter);
+            if (
+              item.type == filter ||
+              item.category == filter ||
+              item.gender == filter
+            ) {
+              return (
+                <Product
+                  img={item.image[0]}
+                  productName={item.model}
+                  productPrice={item.price}
+                />
+              );
+            }
+          })}
+      <LoadMore />
+    </section>
+  );
+};
+
+Data.propTypes = {
+  selected: PropTypes.array,
+};
 
 export default Data;
