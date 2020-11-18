c897d23cd76cfe6b6e9802220d2bc045f78b39f1
biancacraciun <craciunbiancas@yahoo.com>
2020-11-18 13:49:03 +0200

    Revert "get categories from database"
    This reverts commit 6f75ac2aad453e3156fa70a992f0445b20654c61.

--- src/commons/util/data/data.js
+++ src/commons/util/data/data.js
@@ -20,3 +20,48 @@ export const types = [
 ];
 
 export const pages = ["about us", "contact us", "shopping cart", "my account"];
+
+export const checkboxes = [
+  {
+    id: "new-arrivals",
+    name: "new-arrivals",
+    value: "new arrivals",
+  },
+  {
+    id: "men",
+    name: "men",
+    value: "men",
+  },
+  {
+    id: "women",
+    name: "women",
+    value: "women",
+  },
+  {
+    id: "girls",
+    name: "girls",
+    value: "girls",
+  },
+  {
+    id: "boys",
+    name: "boys",
+    value: "boys",
+  },
+  {
+    id: "clothes",
+    name: "clothes",
+    value: "clothes",
+  },
+  {
+    id: "shoes",
+    name: "shoes",
+    value: "shoes",
+  },
+  {
+    id: "accessories",
+    name: "accessories",
+    value: "accessories",
+  },
+];
+
+export const idContext = checkboxes.map((id) => id.id);
