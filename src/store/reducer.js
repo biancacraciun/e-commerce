e92f93c4a1f682216057f113859af2ab7f6a33c1
biancacraciun <craciunbiancas@yahoo.com>
2020-11-18 13:49:56 +0200

    store without action.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,15 +1,46 @@
-import * as actionTypes from "./actions";
+import * as actionTypes from "./actions/actions";
 
 const initialState = {
-  isChecked: false,
+  filters: {
+    isChecked: false,
+    id: [],
+  },
+  categories: [],
+  cart: [],
+};
+
+const remover = (arr, el) => {
+  let index = arr.indexOf(el);
+  return arr.splice(index, 1, el);
+};
+
+const adder = (arr, el) => {
+  arr.push(el);
+  return arr;
+};
+
+const conditional = (bool, arr, el) => {
+  if (bool) {
+    return adder(arr, el);
+  }
+  return remover(arr, el);
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.CHECK_CHECKBOX:
+    case actionTypes.CHECKBOX_FILTER:
       return {
         ...state,
-        isChecked: !state.isChecked,
+        filters: {
+          ...state.filters,
+          isChecked: !state.filters.isChecked,
+          id: conditional(state.filters.isChecked, state.filters.id, action.id),
+        },
+      };
+    case actionTypes.SET_CATEGORIES:
+      return {
+        ...state,
+        categories: action.categories,
       };
     default:
       return state;
